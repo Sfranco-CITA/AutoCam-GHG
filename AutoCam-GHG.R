7e059db54df15e3fe1334d674c97c67f5143e7cc
B0RJA <borja.latorre@eead.csic.es>
2025-04-10 09:29:46 +0200

    Rename variables, improve comments, and move parameters to the top

--- AutoCam-GHG.R
+++ AutoCam-GHG.R
@@ -1,195 +1,225 @@
+################################################################################
+# Script Name: AutoCam-GHG.R
+# Version: 1.0
+# Authors: S. Franco-Luesma, M. Alonso-Ayuso, B. Latorre, J. Álvaro-Fuentes
+# Date: 09/04/2025
+#
+# DESCRIPTION:
+# This R script automates the control of a greenhouse gas (GHG)
+# measurement system using suction chambers connected to a Gasera
+# analyzer and solenoid valves managed via a multiplexer. The system
+# performs scheduled measurements from multiple chambers in defined
+# time cycles and includes rain detection to ensure measurement integrity.
+#
+# CONFIGURATION PARAMETERS (Modify these as needed)
+################################################################################
 
-# initial camera group
-camera_group <- 1
+# Number of chambers per group (controlled together)
+chambers_per_group <- 6
+# Total number of chamber groups
+chamber_groups <- 3
+# Initial group for the measurement cycle
+initial_chamber_group <- 1
 
-# gasera measurement time (in seconds)
-t_meas <- 56
+# Measurement repetitions to perform for each chamber
+measurements_per_chamber <- 5
+# Gasera measurement cycle in seconds
+measurement_duration <- 56
+
+# Threshold for considering it as rain in mm
+rain_threshold <- 0.2
+
+# External dependencies
+rainfall_file <- "C:/Campbellsci/LoggerNet/CR1000XSeries_Datos_90s.csv"
+relay_control_exe <- "\"C:\\Users\\campoMSCG_localAdmin\\Desktop\\programa\\ProjectExes v1.2.1\\RelayCmd.exe\""
+
+# Network configuration
+valve_multiplexer_ip <- "192.168.1.101"
+gasera_analyzer_ip <- "192.168.1.110"
+
+# Logging
+log_file <- "log.txt"
 
 # computer time is GMT
-Sys.setenv(TZ = 'GMT')
+Sys.setenv(TZ = "GMT")
 
-# check for rain
-rain <- function()
-{
-	# read csv
-	pluvio <- read.csv('C:/Campbellsci/LoggerNet/CR1000XSeries_Datos_90s.csv', header=FALSE)
-	# select last column
-	pluvio <- pluvio[ , ncol(pluvio)] 
-	# select last 10 measurements
-	pluvio <- max(tail(pluvio, n=10))
-	# precipitation threshold
-	if(pluvio > 0.2) # 0.2mm
-	{
-		return (TRUE)
-	}
-	return (FALSE)
+# Check for rain
+rain <- function() {
+  # Read rainfall data from CSV
+  pluvio <- read.csv(rainfall_file, header = FALSE)
+  # Select the last column (assuming it contains the rainfall data)
+  pluvio <- pluvio[, ncol(pluvio)]
+  # Select the last 10 measurements and find the maximum
+  pluvio <- max(tail(pluvio, n = 10))
+  # Precipitation threshold check
+  if (pluvio > rain_threshold) {
+    return(TRUE)  # Rain detected
+  }
+  return(FALSE)  # No rain detected
 }
 
-# multiplexer commands
+# Valve multiplexer control
 library(httr2)
-openValve <- function(id)
-{
-	req <- request(paste0("http://192.168.1.101/relayOn?id=", id))
-	req_perform(req)
+open_valve <- function(id) {
+  req <- request(paste0("http://", valve_multiplexer_ip, "/relayOn?id=",
+    id))
+  req_perform(req)
 }
-closeValves <- function()
-{
-	id <- 0
-	req <- request(paste0("http://192.168.1.101/relayOff?id=", id))
-	req_perform(req)
+close_valves <- function() {
+  id <- 0
+  req <- request(paste0("http://", valve_multiplexer_ip, "/relayOff?id=",
+    id))
+  req_perform(req)
 }
 
-# electrovalve relay command
-relay_cmd <- '"C:\\Users\\campoMSCG_localAdmin\\Desktop\\programa\\ProjectExes v1.2.1\\RelayCmd.exe" '
+# Establish a socket connection to the Gasera analyzer
+con <- socketConnection(host = gasera_analyzer_ip, port = 8888, blocking = TRUE,
+  server = FALSE, open = "r+", timeout = 5)
 
-# gasera socket connection
-con <- socketConnection(host="192.168.1.110", port = 8888, blocking=TRUE, server=FALSE, open="r+", timeout=5)
+# Send command to the Gasera analyzer to stop measurements
+write_resp <- writeLines("\002 STPM K0 \003", con)
 
-# stop analyzer
-write_resp <- writeLines("\x02 STPM K0 \x03", con) # stop analyzer
-#cmd <- readLines(con, 1)
-
-# open all cameras
+# Open all chamber groups
 date <- as.POSIXlt(Sys.time(), tz = "UTC")
-cat(paste0(date, " Opening all cameras\n"))
-cmd <- system(paste0(relay_cmd, 'ID=1 OFF=ALL'), intern = TRUE) # open all cameras
+cat(paste0(date, " Opening all chambers\n"))
+cmd <- system(paste0(relay_control_exe, " ID=1 OFF=ALL"), intern = TRUE)
 Sys.sleep(1)
 
-# close all valves
+# Close all valves connected to the multiplexer
 date <- as.POSIXlt(Sys.time(), tz = "UTC")
 cat(paste0(date, " Closing all valves\n"))
-closeValves()  # close all valves
+close_valves()
 
-# open random valve
-random <- sample(1:18, 1)
+# Open a single random valve
+random <- sample(1:(chambers_per_group * chamber_groups), 1)
 cat(paste0(date, " Opening random valve ", random, "\n"))
-openValve(random) # open random valve
+open_valve(random)
 
-while(1)
-{
-	# Wait till next 00m or 30m
-	t_ini <- as.integer(as.integer(Sys.time())/1800)*1800+1800
-	wait <- t_ini - as.integer(Sys.time())
-	cat(paste0(date, " Waiting ", as.integer(wait), " seconds for next group measurement\n"))
-	Sys.sleep(wait)
+# Assign the initial chamber group
+chamber_group <- initial_chamber_group
 
-	# open all cameras
-	date <- as.POSIXlt(Sys.time(), tz = "UTC")
-	cmd <- system(paste0(relay_cmd, 'ID=1 OFF=ALL'), intern = TRUE) # open all cameras
-	Sys.sleep(1)
-	
-	# close camera group
-	if(!(rain()))
-	{
-		cat(paste0(date, " Closing camera group ", camera_group, "\n"))
-		cmd <- system(paste0(relay_cmd, 'ID=1 ON=', camera_group + 4), intern = TRUE) # close camera group i
-		Sys.sleep(1)
-	}
+# Perform one chamber group cycle
+while (1) {
+  # Wait till next 00m or 30m
+  t_ini <- as.integer(as.integer(Sys.time())/1800) * 1800 + 1800
+  wait <- t_ini - as.integer(Sys.time())
+  cat(paste0(date, " Waiting ", as.integer(wait), " seconds to start next chamber group measurement\n"))
+  Sys.sleep(wait)
 
-	# close all valves
-	date <- as.POSIXlt(Sys.time(), tz = "UTC")
-	cat(paste0(date, " Closing all valves\n"))
-	closeValves()  # close all valves
+  # Open all chamber groups
+  date <- as.POSIXlt(Sys.time(), tz = "UTC")
+  cmd <- system(paste0(relay_control_exe, " ID=1 OFF=ALL"), intern = TRUE)
+  Sys.sleep(1)
 
-	# open initial valve
-	multiplexer  <- (camera_group - 1) * 6 + 1
-	cat(paste0(date, " Opening valve ", multiplexer, "\n"))
-	openValve(multiplexer) # open valve
-	
-	# Delay to evacuate the line
-	Sys.sleep(40)
+  # Close current chamber group (unless it's raining)
+  if (!(rain())) {
+    cat(paste0(date, " Closing chamber group ", chamber_group, "\n"))
+    cmd <- system(paste0(relay_control_exe, " ID=1 ON=", chamber_group +
+      4), intern = TRUE)
+    Sys.sleep(1)
+  }
 
-	# Connect with the analyzer and start measurements
-	if(!(rain()))
-	{
-		write_resp <- writeLines("\x02 STAM K0 23 \x03", con)
-		cmd <- readLines(con, 1)
-	}
-	
-	# group start time
-	t_ini <- as.integer(Sys.time())
+  # Close all valves connected to the multiplexer
+  date <- as.POSIXlt(Sys.time(), tz = "UTC")
+  cat(paste0(date, " Closing all valves\n"))
+  close_valves()
 
-	# for each measurement
-	for (meas in 1:5) 
-	{
-		# for each camera
-		for(camera in 1:6)
-		{
-			# Delay to finish suction
-			Sys.sleep(6)
-			
-			# advance valve to preare next suction
-			valve <- multiplexer + camera
-			if(camera == 6)
-			{
-				valve <- multiplexer
-			}
-			
-			# close all valves
-			closeValves()  # close all valves
-			
-			# open next valve
-			date <- as.POSIXlt(Sys.time(), tz = "UTC")
-			cat(paste0(date, " Opening valve ", valve, "\n"))
-			openValve(valve) # open valve
-			
-			# wait for the measurement to finish
-			wait <- t_ini + ( camera + ( meas - 1 ) * 6 ) * t_meas - as.integer(Sys.time())
-			cat(paste0(date, " Waiting ", as.integer(wait), " seconds\n"))
-			Sys.sleep(wait)
-			
-			# Delay to finish processing
-			Sys.sleep(4)
-			
-			# read measurement
-			write_resp <- writeLines("\x02 ACON K0 \x03", con)
-			cmd <- readLines(con, 1)
-			date <- as.POSIXlt(Sys.time(), tz = "UTC")
-			cat(paste0(date, " C", camera, " B", camera_group, " ", cmd, "\n"))
-			cat(file="prueba_1.txt", append=TRUE, paste0(date, " C", camera, " B", camera_group, " rain=", rain(), " ", cmd, "\n"))
-		
-			if(rain())
-			{
-				# stop analyzer
-				write_resp <- writeLines("\x02 STPM K0 \x03", con) # stop analyzer
-				#cmd <- readLines(con, 1)
-				
-				# open all cameras
-				date <- as.POSIXlt(Sys.time(), tz = "UTC")
-				cat(paste0(date, " Opening all cameras\n"))
-				cmd <- system(paste0(relay_cmd, 'ID=1 OFF=ALL'), intern = TRUE) # open all cameras
-				Sys.sleep(1)
-			}
-		}
-	} 
+  # Open the first valve within the current chamber group
+  multiplexer <- (chamber_group - 1) * chambers_per_group + 1
+  cat(paste0(date, " Opening valve ", multiplexer, "\n"))
+  open_valve(multiplexer)
 
-	# stop analyzer
-	write_resp <- writeLines("\x02 STPM K0 \x03", con) # stop analyzer
-	#cmd <- readLines(con, 1)
+  # Delay to allow the gas line to evacuate
+  Sys.sleep(40)
 
-	# open all cameras
-	date <- as.POSIXlt(Sys.time(), tz = "UTC")
-	cat(paste0(date, " Opening all cameras\n"))
-	cmd <- system(paste0(relay_cmd, 'ID=1 OFF=ALL'), intern = TRUE) # open all cameras
-	Sys.sleep(1)
+  # Connect with the analyzer and start measurements (unless it's raining)
+  if (!(rain())) {
+    write_resp <- writeLines("\002 STAM K0 23 \003", con)
+    cmd <- readLines(con, 1)
+  }
 
-	# close all valves
-	date <- as.POSIXlt(Sys.time(), tz = "UTC")
-	cat(paste0(date, " Closing all valves\n"))
-	closeValves()  # close all valves
-	
-	# open random valve
-	random <- sample(1:18, 1)
-	cat(paste0(date, " Opening random valve ", random, "\n"))
-	openValve(random) # open valve
+  # Record the start time of the current measurement group
+  t_ini <- as.integer(Sys.time())
 
-	# advance group 
-	camera_group <- camera_group + 1
-	if(camera_group == 4)
-	{
-		camera_group <- 1
-	}
+  # Measurement repetitions for each chamber
+  for (measurement in 1:measurements_per_chamber) {
+    # Loop through chambers within the current group
+    for (chamber in 1:chambers_per_group) {
+      # Wait to ensure suction is complete for the current chamber
+      Sys.sleep(6)
+
+      # Determine the valve to open for the next suction
+      valve <- multiplexer + chamber
+      if (chamber == chambers_per_group) {
+        valve <- multiplexer
+      }
+
+      # Close all valves connected to the multiplexer
+      close_valves()
+
+      # Open next valve
+      date <- as.POSIXlt(Sys.time(), tz = "UTC")
+      cat(paste0(date, " Opening valve ", valve, "\n"))
+      open_valve(valve)  # open valve
+
+      # Calculate the remaining wait time until the expected measurement completion
+      wait <- t_ini + (chamber + (measurement - 1) * chambers_per_group) *
+        measurement_duration - as.integer(Sys.time())
+      cat(paste0(date, " Waiting ", as.integer(wait), " seconds\n"))
+      Sys.sleep(wait)
+
+      # Small delay to allow internal processing after measurement
+      Sys.sleep(4)
+
+      # Request and read the measurement data
+      write_resp <- writeLines("\002 ACON K0 \003", con)
+      cmd <- readLines(con, 1)
+      date <- as.POSIXlt(Sys.time(), tz = "UTC")
+      cat(paste0(date, " C", chamber, " G", chamber_group, " ", cmd,
+        "\n"))
+      cat(file = log_file, append = TRUE, paste0(date, " C", chamber,
+        " G", chamber_group, " rain=", rain(), " ", cmd, "\n"))
+
+  		# If it's raining
+      if (rain()) {
+        # Send command to the Gasera analyzer to stop measurements
+        write_resp <- writeLines("\002 STPM K0 \003", con)
+
+        # Open all chamber groups
+        date <- as.POSIXlt(Sys.time(), tz = "UTC")
+        cat(paste0(date, " Opening all chambers\n"))
+        cmd <- system(paste0(relay_control_exe, " ID=1 OFF=ALL"),
+          intern = TRUE)
+        Sys.sleep(1)
+      }
+    }
+  }
+
+  # Send command to the Gasera analyzer to stop measurements
+  write_resp <- writeLines("\002 STPM K0 \003", con)  # stop analyzer
+
+  # Open all chamber groups
+  date <- as.POSIXlt(Sys.time(), tz = "UTC")
+  cat(paste0(date, " Opening all chambers\n"))
+  cmd <- system(paste0(relay_control_exe, " ID=1 OFF=ALL"), intern = TRUE)
+  Sys.sleep(1)
+
+  # Close all valves connected to the multiplexer
+  date <- as.POSIXlt(Sys.time(), tz = "UTC")
+  cat(paste0(date, " Closing all valves\n"))
+  close_valves()
+
+  # Open a single random valve
+  random <- sample(1:(chambers_per_group * chamber_groups), 1)
+  cat(paste0(date, " Opening random valve ", random, "\n"))
+  open_valve(random)
+
+  # Determine the next chamber group
+  chamber_group <- chamber_group + 1
+  if (chamber_group == chamber_groups + 1) {
+    chamber_group <- 1
+  }
 }
 
+# Close connection to the Gasera analyzer
 close(con)
-
